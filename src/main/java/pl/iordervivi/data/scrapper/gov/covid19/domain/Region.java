812fc3e416d141bda1b67ed4a4628f26e900f6b1
Dima Lumelskyj <14013882+DimaLumelskyj@users.noreply.github.com>
2020-03-17 18:48:09 +0100

    commenting county property in Region domain due to - data provider not give this information

--- src/main/java/pl/iordervivi/data/scrapper/gov/covid19/domain/Region.java
+++ src/main/java/pl/iordervivi/data/scrapper/gov/covid19/domain/Region.java
@@ -27,10 +27,12 @@ public class Region {
     @NotBlank(message = "Region cannot be null/blank.")
     private String region;
 
+    /*
     @Column(nullable = false)
     @Size(max = 256, message = "County character limit is 256.")
     @NotBlank(message = "County cannot be null/blank.")
     private String county;
+    */
 
     @OneToMany(mappedBy = "region", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     //@OrderBy("seatRow, seatNumber")
