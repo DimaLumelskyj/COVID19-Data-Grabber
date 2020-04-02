d94398e493a431cf9798ecf198ed06bbea99dbbc
Dima Lumelskyj <14013882+DimaLumelskyj@users.noreply.github.com>
2020-04-02 17:00:28 +0200

    update logger

--- src/main/java/pl/iordervivi/data/scrapper/gov/covid19/config/LogResourceText.java
+++ src/main/java/pl/iordervivi/data/scrapper/gov/covid19/config/LogResourceText.java
@@ -9,4 +9,7 @@ public class LogResourceText {
    public final static String WEB_SCRAPPING_STARTED = "Data web scrapping for covid-19 disease starting";
    public final static String JOB_SCHEDULER_STARTED = "Scheduler starting jobs:";
    public final static String JOB_TRIGGER_STARTED = "running: jobTrigger";
+   public final static String DATA_SCRAPPING_STARTED = "Scrapping...";
+   public final static String DATA_SCRAPPING_SAME_DATA = "There is the same data in previous step so not adding to the database";
+   public final static String DATA_SCRAPPING_COMPLETE = "Parsing complete";
 }
--- src/main/java/pl/iordervivi/data/scrapper/gov/covid19/service/DataProviderImpl.java
+++ src/main/java/pl/iordervivi/data/scrapper/gov/covid19/service/DataProviderImpl.java
@@ -4,16 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.DeserializationProblemHandler;
 import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import pl.iordervivi.data.scrapper.gov.covid19.config.ApplicationProperties;
+import pl.iordervivi.data.scrapper.gov.covid19.config.LogResourceText;
 import pl.iordervivi.data.scrapper.gov.covid19.dto.SickInRegionsDto;
-import pl.iordervivi.data.scrapper.gov.covid19.job.Covid19PolandDataScrapperJob;
 import pl.iordervivi.data.scrapper.gov.covid19.util.ErrorHandlerUtility;
 
 import javax.transaction.Transactional;
@@ -25,45 +25,39 @@ import java.util.Optional;
 
 @Service
 @Transactional
+@Slf4j
+@RequiredArgsConstructor
 public class DataProviderImpl implements DataProvider {
-    private final static Logger logger = LoggerFactory.getLogger(Covid19PolandDataScrapperJob.class);
+
     private final static ObjectMapper mapper = new ObjectMapper();
+    private final ApplicationProperties applicationProperties;
     private final RegionService regionService;
     private final SickService sickService;
     private final SickInRegionsDtoService sickInRegionsDtoService;
-    @Value("${data.url.provider}")
-    private String dataWebScrapUrl;
 
-    public DataProviderImpl(RegionService regionService,
-                            SickService sickService,
-                            SickInRegionsDtoService sickInRegionsDtoService) {
-        this.regionService = regionService;
-        this.sickService = sickService;
-        this.sickInRegionsDtoService = sickInRegionsDtoService;
-    }
 
     @Override
     public void addSicksByRegion() {
-        logger.info("Scrapping...");
+        log.info(LogResourceText.DATA_SCRAPPING_STARTED);
         try {
             long step = sickService.getCurrentStep();
             List<SickInRegionsDto> sickInRegionsDtoList = webScrapperTry();
             Optional<SickInRegionsDto> totalSickInRegionsDto = sickInRegionsDtoService.ifExistsTotalSickEntryInRegionDtoObjectList(sickInRegionsDtoList);
             if (step > 1 && totalSickInRegionsDto.isPresent() && sickService.ifSameDataInPreviousStep(totalSickInRegionsDto, step)) {
-                logger.info("There is the same data in previous step so not adding to the database");
+                log.info(LogResourceText.DATA_SCRAPPING_SAME_DATA);
                 return;
             }
             regionService.addRegions(sickInRegionsDtoList);
             LocalDateTime now = LocalDateTime.now();
             sickService.addSick(sickInRegionsDtoList, now, step);
-            logger.info("Parsing complete");
+            log.info(LogResourceText.DATA_SCRAPPING_COMPLETE);
         } catch (IOException e) {
-            logger.error(Arrays.toString(e.getStackTrace()));
+            log.error(Arrays.toString(e.getStackTrace()));
         }
     }
 
     private List<SickInRegionsDto> webScrapperTry() throws IOException {
-        Document document = Jsoup.connect(dataWebScrapUrl).get();
+        Document document = Jsoup.connect(applicationProperties.getDataProviderUrl()).get();
         Element formElement = document.select("pre").first();
         String jsonData = StringUtils.substringBetween(formElement.toString(), "\"parsedData\":\"", "\",\"fileName\"");
         jsonData = StringUtils.remove(jsonData, "\\");
