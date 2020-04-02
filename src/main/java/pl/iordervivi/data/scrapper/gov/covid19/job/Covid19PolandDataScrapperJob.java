7f28c34bbf3d2d48507f96050a611b311d1e1095
Dima Lumelskyj <14013882+DimaLumelskyj@users.noreply.github.com>
2020-04-02 16:46:08 +0200

    update job DI

--- src/main/java/pl/iordervivi/data/scrapper/gov/covid19/job/Covid19PolandDataScrapperJob.java
+++ src/main/java/pl/iordervivi/data/scrapper/gov/covid19/job/Covid19PolandDataScrapperJob.java
@@ -4,21 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import pl.iordervivi.data.scrapper.gov.covid19.config.LogResourceText;
 import pl.iordervivi.data.scrapper.gov.covid19.service.DataProvider;
 
-import javax.transaction.Transactional;
-
 @Component
-@Transactional
 @Slf4j
 public class Covid19PolandDataScrapperJob implements Job {
 
-    //TODO: fix in future with static constructor and interface throw error
     @Autowired
-    @Qualifier("dataProviderImpl")
     private DataProvider dataProvider;
 
     @Override
