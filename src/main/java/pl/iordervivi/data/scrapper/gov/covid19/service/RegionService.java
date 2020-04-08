5e60203cbc534dcf0de67571bc0788fed438bf58
Dima Lumelskyj <14013882+DimaLumelskyj@users.noreply.github.com>
2020-04-08 20:04:14 +0200

    added lombok annotation instead of constructor in RegionService class

--- src/main/java/pl/iordervivi/data/scrapper/gov/covid19/service/RegionService.java
+++ src/main/java/pl/iordervivi/data/scrapper/gov/covid19/service/RegionService.java
@@ -1,5 +1,6 @@
 package pl.iordervivi.data.scrapper.gov.covid19.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.iordervivi.data.scrapper.gov.covid19.domain.Region;
 import pl.iordervivi.data.scrapper.gov.covid19.dto.DiseaseStatisticInRegionDto;
@@ -8,12 +9,9 @@ import pl.iordervivi.data.scrapper.gov.covid19.repo.RegionRepository;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class RegionService {
     private final RegionRepository regionRepository;
-
-    public RegionService(RegionRepository regionRepository) {
-        this.regionRepository = regionRepository;
-    }
 
     void addRegions(List<DiseaseStatisticInRegionDto> DiseaseStatisticInRegionsDto) {
         for (DiseaseStatisticInRegionDto DiseaseStatisticInRegionDTO : DiseaseStatisticInRegionsDto) {
