1c9dd01acc2660ab7979919b6f50f8ac219ab8df
Dima Lumelskyj <14013882+DimaLumelskyj@users.noreply.github.com>
2020-04-19 21:53:49 +0200

    update to logger

--- src/main/java/pl/iordervivi/data/scrapper/gov/covid19/util/UnMarshallingErrorHandler.java
+++ src/main/java/pl/iordervivi/data/scrapper/gov/covid19/util/UnMarshallingErrorHandler.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.deser.DeserializationProblemHandler;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
+@Slf4j
 public class UnMarshallingErrorHandler extends DeserializationProblemHandler {
     @Override
     public boolean handleUnknownProperty(DeserializationContext ctxt,
@@ -15,7 +17,7 @@ public class UnMarshallingErrorHandler extends DeserializationProblemHandler {
                                          Object beanOrClass,
                                          String propertyName) throws IOException {
         super.handleUnknownProperty(ctxt, jp, deserializer, beanOrClass, propertyName);
-        System.out.println("Property with name '" + propertyName + "' doesn't exist in Class of type '" + beanOrClass.getClass().getName() + "'");
+        log.error("Property with name '" + propertyName + "' doesn't exist in Class of type '" + beanOrClass.getClass().getName() + "'");
         return true;
     }
 }
